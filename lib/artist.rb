b083d2e7e5568b3748a55ba8774a062f251460da
Maurice Fiorani <mauricef97@gmail.com>
2020-02-18 18:16:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,7 +12,6 @@ class Artist
   end
   
   def add_song(song)
-    self.songs << song
     song.artist = self
     @@song_count += 1 
     
@@ -21,7 +20,6 @@ class Artist
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     song.artist = self
-    self.songs << song
     @@song_count +=1
   end
   
